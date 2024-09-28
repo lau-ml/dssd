e7aa53d8880b94e8e74e1302c6387ad657cf3bf4
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2024-09-28 00:23:41 -0300

    Agregado de recursos

---
+++ cliente/src/recoleccion_cargar/recoleccion_cargar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recoleccion-cargar',
+  standalone: true,
+  imports: [],
+  templateUrl: './recoleccion_cargar.component.html',
+  styleUrl: './recoleccion_cargar.component.css'
+})
+export class Recoleccion_cargarComponent {
+
+}
