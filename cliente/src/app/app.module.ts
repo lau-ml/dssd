b9f9ae1cdfd0ee8aa85bd9b0d389e604c2f9f3c9
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2024-09-28 00:23:05 -0300

    Agregado de componentes

---
+++ cliente/src/app/app.module.ts
@@ -0,0 +1,13 @@
+import {NgModule} from "@angular/core";
+import {NavComponent} from "../nav/nav.component";
+import {HomeComponent} from "../home/home.component";
+import {Recoleccion_cargarComponent} from "../recoleccion_cargar/recoleccion_cargar.component";
+
+@NgModule({
+  imports: [],
+  declarations: [
+    HomeComponent,
+    NavComponent,
+    Recoleccion_cargarComponent
+  ],
+})
