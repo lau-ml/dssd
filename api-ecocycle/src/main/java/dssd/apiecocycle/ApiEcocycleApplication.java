3bbe7535f0536a8b8157bb65895fa3c63240fb1b
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2024-10-09 15:08:44 -0300

    Add api

---
+++ api-ecocycle/src/main/java/dssd/apiecocycle/ApiEcocycleApplication.java
@@ -0,0 +1,13 @@
+package dssd.apiecocycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiEcocycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiEcocycleApplication.class, args);
+	}
+
+}
