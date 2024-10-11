10a333a3bbb8545020c3db08e801f538e2aae67e
02Nico02 <ika.22.94@gmail.com>
2024-10-11 15:08:32 -0300

    Elimine importaciones que no se usaban

--- api-ecocycle/src/main/java/dssd/apiecocycle/model/Centro.java
+++ api-ecocycle/src/main/java/dssd/apiecocycle/model/Centro.java
@@ -13,8 +13,6 @@ import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.ManyToOne;
 
-import java.util.HashSet;
-
 import lombok.Getter;
 import lombok.Setter;
 
