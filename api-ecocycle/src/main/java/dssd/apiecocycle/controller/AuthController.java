2b5b39f2788d7b7368690c85cfae9565b336a22f
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2024-10-17 12:06:11 -0300

    Credenciales inválidas

--- api-ecocycle/src/main/java/dssd/apiecocycle/controller/AuthController.java
+++ api-ecocycle/src/main/java/dssd/apiecocycle/controller/AuthController.java
@@ -1,7 +1,6 @@
 package dssd.apiecocycle.controller;
 
 import dssd.apiecocycle.exceptions.CentroInvalidoException;
-
 import dssd.apiecocycle.requests.LoginRequest;
 import dssd.apiecocycle.requests.RegisterRequest;
 import dssd.apiecocycle.response.AuthResponse;
@@ -15,7 +14,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -35,7 +33,22 @@ public class AuthController {
     @Operation(summary = "Iniciar sesión", description = "Este endpoint permite a los centros de recolección y depositos iniciar sesión con sus credenciales. Devuelve un token JWT para acceder a los endpoints protegidos.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Inicio de sesión exitoso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponse.class), examples = @ExampleObject(value = "{\"token\": \"jwt_token_aqui\"}"))),
-            @ApiResponse(responseCode = "400", description = "Credenciales inválidas", content = @Content(mediaType = "application/json", examples = @ExampleObject(value = "{\"message\": \"Email o contraseña incorrectos\"}"))),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Datos inválidos: El email o la contraseña están vacíos",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(value = "{\"errors\": [\"El mail del centro no puede estar vacío\", \"La contraseña no puede estar vacía.\"]}")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Credenciales inválidas",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(value = "{\"message\": \"Email o contraseña incorrectos.\"}")
+                    )
+            ),
             @ApiResponse(
                     responseCode = "500",
                     description = "Error interno del servidor",
@@ -43,21 +56,38 @@ public class AuthController {
                             mediaType = "application/json",
                             examples = @ExampleObject(value = "{\"message\": \"Error interno del servidor.\"}")
                     )
-            )    })
+            )})
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
 
         try {
             return new ResponseEntity<AuthResponse>(centroService.login(request), HttpStatus.OK);
         } catch (CentroInvalidoException e) {
-            return new ResponseEntity<>(MessageResponse.builder().message(e.getMessage()).build(),HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(MessageResponse.builder().message(e.getMessage()).build(), HttpStatus.UNAUTHORIZED);
         }
     }
 
     @Operation(summary = "Registro de nuevo centro de recolección", description = "Este endpoint permite registrar un nuevo centro de recolección proporcionando los datos requeridos.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Registro exitoso", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MessageResponse.class), examples = @ExampleObject(value = "{\"message\": \"Registro exitoso.\"}"))),
-            @ApiResponse(responseCode = "400", description = "Datos inválidos", content = @Content(mediaType = "application/json", schema = @Schema(implementation = MessageResponse.class), examples = @ExampleObject(value = "El centro ingresado ya existe"))),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Datos inválidos",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MessageResponse.class),
+                            examples = @ExampleObject(value = "{\"errors\": [\"El email ya está registrado.\", \"La contraseña no puede estar vacía.\"]}")
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "409",
+                    description = "Conflicto: El centro ya existe",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = MessageResponse.class),
+                            examples = @ExampleObject(value = "{\"message\": \"El centro ingresado ya existe.\"}")
+                    )
+            ),
             @ApiResponse(
                     responseCode = "500",
                     description = "Error interno del servidor",
@@ -68,11 +98,11 @@ public class AuthController {
             )
     })
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) throws UnsupportedEncodingException{
-        try{
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) throws UnsupportedEncodingException {
+        try {
             return new ResponseEntity<>(centroService.register(request), HttpStatus.OK);
         } catch (CentroInvalidoException e) {
-            return new ResponseEntity<>(MessageResponse.builder().message(e.getMessage()).build(),HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(MessageResponse.builder().message(e.getMessage()).build(), HttpStatus.CONFLICT);
         }
 
     }
