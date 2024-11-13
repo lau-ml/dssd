c7f8b8fe1a46896e98222a2b48077ae646b652e9
Lautaro Moller <lautaro.moller225010@alumnos.info.unlp.edu.ar>
2024-11-13 19:14:35 -0300

    Agrego cron jobs. Queda realizar ajustes y pruebas de funcionamiento

--- api-sorteo-kubernetes/src/main/java/dssd/apisorteokubernetes/ApiSorteoKubernetesApplication.java
+++ api-sorteo-kubernetes/src/main/java/dssd/apisorteokubernetes/ApiSorteoKubernetesApplication.java
@@ -1,7 +1,9 @@
 package dssd.apisorteokubernetes;
 
+import dssd.apisorteokubernetes.helpers.SorteoHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -9,7 +11,26 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ApiSorteoKubernetesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiSorteoKubernetesApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(ApiSorteoKubernetesApplication.class, args);
+        SorteoHelper helper = context.getBean(SorteoHelper.class);
+        String task = System.getenv("TASK");
+
+        if (task != null) {
+            switch (task) {
+                case "realizarSorteo":
+                    helper.realizarSorteo();
+                    break;
+                case "inhabilitarInscripciones":
+                    helper.inhabilitarInscripciones();
+                    break;
+                case "crearSorteo":
+                    helper.crearSorteo();
+                    break;
+                default:
+                    System.out.println("Tarea no reconocida");
+            }
+        }
+
     }
 
 }
